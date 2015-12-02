b335b7b1c702ed9e0c45da200df7c97aa9e6c38a
Harry Sadler <harrysadlermusic@gmail.com>
2015-12-02 08:30:17 -0800

    add mongodb connection via mongoose in server.js, update package.json, remove bluebird dependency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 app.use(bodyParser.json());
@@ -12,11 +13,25 @@ var port = process.env.PORT || 8080;
 var node_env = process.env.NODE_ENV || 'development';
 
 
+// MongoDB connection
+mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/ZenType';
+
+mongoose.connect(mongoURI);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('db success');
+});
+
+
+// serve static files from specified locations and index page on "/"
 app.use(express.static(__dirname + '/../client/static'));
 app.use(express.static(__dirname + '/../client/build'));
 app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
+
 
 if (require.main === module) {
   app.listen(port);
